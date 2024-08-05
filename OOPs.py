6004be534c76be6a3d7913e0dcf661ccd339dc7b
Zahid672 <zeeuom@gmail.com>
2024-08-05 17:16:39 +0900

    OOPS in python

---
+++ OOPs.py
@@ -0,0 +1,21 @@
+""" procedural ---- > Functional ----- > OOPS... 
+Class is a blueprint for creating objects."""
+
+# creating class
+# class Student:
+#     name = "Zahid"
+
+# # creating object(instance), object is also called instance of class.
+# s1 = Student()
+# print(s1.name)
+
+# class Car:
+#     color = "white" #parameters
+#     brand = "tesla" 
+
+# car1 = Car()
+# print(car1.color)
+# print(car1.brand)
+
+"""__init__ Function (constructor) All classes have a function called
+__init__(), which is always executed when the class is being initiated."""
