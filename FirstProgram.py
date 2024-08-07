45e78bb5a5e1c176e9c0e1ff7fe5a9bfd3a91a8f
Zahid672 <zeeuom@gmail.com>
2024-08-07 17:14:20 +0900

    Variables and Data Types

---
+++ FirstProgram.py
@@ -0,0 +1,16 @@
+# First Program
+print("Hello World")
+
+# Python Character Set
+"""Letters - A to Z, a to z
+Digits- 0 to 9
+Special Symbols -, +, *, /, etc.
+Whitespaces - Blank Space, tab, carriage return, newline, formfeed
+- Other Characters - Python can process all ASCII and Unicode 
+characters as part of data or literals"""
+
+
+""""Variables: A variable is a name given to a memory location in a program"""
+name = "Zahid" # string
+age = 44
+print("My name is:", name, "and my age is: ", age)
