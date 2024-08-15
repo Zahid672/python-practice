f125da07fa82909da90e3a15ac30820f8313bb9a
Zahid672 <zeeuom@gmail.com>
2024-08-15 16:24:52 +0900

    If Statement

---
+++ Conditional Statements/if_elif.py
@@ -0,0 +1,13 @@
+light = "green"
+
+if(light == "red"):
+    print("Stop")
+
+elif(light == "green"):
+    print("Go")
+elif(light == "yellow"):
+    print("Look")
+
+print("end of code")
+
+
---
+++ Conditional Statements/if_elif_else_statement.py
@@ -0,0 +1,12 @@
+light = "yellow"
+
+if(light == "green"):
+    print("Go")
+
+elif(light == "red"):
+    print("Stop")
+elif(light == "yellow"):
+    print("Look")
+
+else:
+    print("Light is broken")
