630f0663d43321fabd72ddfdcb83f51785256074
Zahid672 <zeeuom@gmail.com>
2024-10-08 12:49:52 +0900

    Constructor

---
+++ OOPS/Class_and_Instance_attributes.py
@@ -0,0 +1,33 @@
+class Student:
+    college_name = "Mardan Model" ### class attribute
+
+    def __init__(self, name, marks):
+        self.name = name ### object attribute
+        self.marks = marks
+        print("adding new student in Database..")
+
+s1 = Student("Zahid", 99)
+print(s1.name, s1.marks)
+
+s2 = Student("Saeed", 100)
+print(s2.name, s2.marks)
+
+s3 = Student("Imran", 100)
+print(s3.name,s3.marks)
+
+print(s3.college_name)
+
+
+####### when there is same name attribute (variable name) in class and object, then the precedence (priority) of object attribute is higher than class attribute. For example:
+class Student:
+    college_name = "MMS&C" 
+    name = "ananymous"  ### class attribute
+
+    def __init__(self, name, marks):
+        self.name = name ### object attribute (In the output we will get this output name, object attribute > class atrribute)
+        self.marks = marks
+        print("adding new student..")
+
+s1 = Student("Khan", 88)
+print(s1.name, s1.marks)
+
