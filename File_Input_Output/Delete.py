cbba08220a82e8472d6da9f500d559fe177c4104
Zahid672 <zeeuom@gmail.com>
2024-10-16 16:04:39 +0900

    How to delete a file

---
+++ File_Input_Output/Delete.py
@@ -0,0 +1,4 @@
+### Deleting a File using the OS module: Module (like a code library (pre-install)) is a file written by another programmer that generally has a functions we can use.
+import os
+
+os.remove("sample.txt")
