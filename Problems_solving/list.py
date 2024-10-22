211654beaa6253e4d59a15e16991ea182252b8bf
Zahid672 <zeeuom@gmail.com>
2024-10-22 17:21:58 +0900

    Calculate the sum and average of numbers in a list

--- Problems_solving/list.py
+++ Problems_solving/list.py
@@ -10,3 +10,17 @@ print("\nYour favorite fruits are:")
 print(fav_fruits)
 
 
+
+
+### Find the sum and average of numbers in a list: Create a list of 10 numbers. Write a program to calculate and print the sum and average of these numbers.
+
+list = [1, 3, 2, 4, 5, 6, 7, 8, 9, 10]
+
+
+
+numbers_list = sum(list)
+
+avg_num = numbers_list / len(list)
+
+print(numbers_list)
+print(avg_num)
