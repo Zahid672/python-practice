eaa2beacad05bc2d642884eba14f1a3f5d134fb5
Zahid672 <zeeuom@gmail.com>
2024-10-28 17:12:55 +0900

    print number from 1 to 10

---
+++ Problems_solving/For_loop/for_loop.py
@@ -0,0 +1,4 @@
+###Print numbers from 1 to 10: Write a program that uses a for loop to print numbers from 1 to 10.
+
+for number in range(1, 11):
+    print(number)
--- Problems_solving/list.py
+++ Problems_solving/list.py
@@ -1,26 +1,51 @@
 ##Create a list of favorite fruits: Write a program that asks the user to enter their 5 favorite fruits and stores them in a list. Then print the list.
 
-fav_fruits = []
+# fav_fruits = []
 
-for i in range(5):
-    fruit = input(f"Enter your favorite fruit #{i+1}: ")
-    fav_fruits.append(fruit)
+# for i in range(5):
+#     fruit = input(f"Enter your favorite fruit #{i+1}: ")
+#     fav_fruits.append(fruit)
 
-print("\nYour favorite fruits are:")
-print(fav_fruits)
+# print("\nYour favorite fruits are:")
+# print(fav_fruits)
 
 
 
 
 ### Find the sum and average of numbers in a list: Create a list of 10 numbers. Write a program to calculate and print the sum and average of these numbers.
 
-list = [1, 3, 2, 4, 5, 6, 7, 8, 9, 10]
+# list = [1, 3, 2, 4, 5, 6, 7, 8, 9, 10]
 
 
 
-numbers_list = sum(list)
+# numbers_list = sum(list)
 
-avg_num = numbers_list / len(list)
+# avg_num = numbers_list / len(list)
 
-print(numbers_list)
-print(avg_num)
+# print(numbers_list)
+# print(avg_num)
+
+
+### Remove duplicates from a list: Given a list with duplicate elements, write a program to remove the duplicates and print the new list.
+
+# original_list = [2, 4, 2, 4, 4, 5, 5, 6, 7]
+
+
+# unique_list_ordered = []
+
+# for item in original_list:
+#     if item not in unique_list_ordered:
+#         unique_list_ordered.append(item)
+
+
+# print(original_list)
+
+# print(unique_list_ordered)
+
+
+####### Reverse a list: Create a list of 5 elements. Write a program to reverse the order of elements in the list and print the reversed list.
+
+### Create a list of favorite fruits: Write a program that asks the user to enter their 5 favorite fruits and stores them in a list. Then print the list.
+
+
+
