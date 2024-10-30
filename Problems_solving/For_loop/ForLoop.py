fa1b2813db90727d8d5a7ab1014f93462e0e6013
Zahid672 <zeeuom@gmail.com>
2024-10-30 16:03:18 +0900

    print numbers from 1 to 100

--- Problems_solving/For_loop/ForLoop.py
+++ Problems_solving/For_loop/ForLoop.py
@@ -1,7 +1,7 @@
 ###Calculate the sum of numbers from 1 to 100: Use a for loop to calculate and print the sum of all numbers from 1 to 100.
 sum = 0
 
-for number in range(1, 100):
+for number in range(1, 101):
     sum += number
 
-print("The sum of number from 1 to 100 is: ", sum)
+print(f"The sum of numbers from 1 to 100 is:", sum)
