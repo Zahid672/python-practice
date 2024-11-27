04ed60bf672f432f416207b6f09301bb8ba5465d
Zahid672 <zeeuom@gmail.com>
2024-11-27 16:19:59 +0900

    Student dictionary with keys for name, age, and grade

---
+++ Dictionary_and_Set/dict_user_input.py
@@ -0,0 +1,8 @@
+student = {}
+
+student["name"] = input("Enter the student's name: ")
+student["age"] = int(input("Enter the student's age: "))
+student["grade"] = int(input("Enter the student's grade: "))
+
+print("\nStudent Information:")
+print(student)
