00150acf9a032552a91933ac46204ffc957af063
Zahid672 <zeeuom@gmail.com>
2024-12-12 15:25:49 +0900

    pass statement in empty class definition to avoid getting an error.

---
+++ OOPS/Pass_statement.py
@@ -0,0 +1,5 @@
+class Person:
+    """class definitions cannot be empty, but if you for some reason have
+    a class definition with no content, put in the pass statement to avoid getting an error."""
+    
+    pass
