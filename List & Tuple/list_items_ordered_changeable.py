ba2ad10e85b4f5f611a317b8ceb09f14ddbf8fd9
Zahid672 <zeeuom@gmail.com>
2024-12-16 16:37:01 +0900

    duplicate values in list

---
+++ List & Tuple/list_items_ordered_changeable.py
@@ -0,0 +1,11 @@
+"""List items are ordered, changeable, and allow duplicate values.
+
+List items are indexed, the first item has index [0], the second item has index [1] etc.
+Ordered:lists are ordered, it means that the items have a defined order, and that order will not change.
+
+If we add new items to a list, the new items will be placed at the end of the list
+Changeable: The list is changeable, meaning that we can change, add, and remove items in a list after it has been created.
+Allow duplicates: Since lists are indexed, lists can have items with the same value:"""
+
+mylist = ["apple", "banana", "banana", "apple", "apple" ]
+print(mylist)
