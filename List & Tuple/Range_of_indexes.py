3af695430423f341a0359519e91387b65b9b4d16
Zahid672 <zeeuom@gmail.com>
2024-12-17 16:56:02 +0900

    indexing

---
+++ List & Tuple/Range_of_indexes.py
@@ -0,0 +1,12 @@
+"""We can specify a range of indexes by specifying 
+where to start and where to end the range. When specifying a range, the return value will be a new list with the specified items."""
+
+mylist = ["apple", "banana", "orange", "pomegranate", "dryfruits"]
+
+print(mylist[1:3])
+
+print(mylist[:2])
+
+print(mylist[:])
+
+print(mylist[2:])
