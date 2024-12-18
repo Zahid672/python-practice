33fb91b2a35cdabdf853ac660e1e4cd4ee0e13e3
Zahid672 <zeeuom@gmail.com>
2024-12-18 17:15:54 +0900

    insert method to insert a new value on a specific index

---
+++ List & Tuple/insert_items.py
@@ -0,0 +1,5 @@
+## To insert a new list item, without replacing any of the existing values, we can use the insert() method.
+mylist = ["apple", "banana", "cherry"]
+mylist.insert(2, "watermelon")
+
+print(mylist)
