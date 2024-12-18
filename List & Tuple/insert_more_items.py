b6c86e8e02c49af2d8765355b0d44bce9c2aeb6d
Zahid672 <zeeuom@gmail.com>
2024-12-18 17:08:39 +0900

    insert less items than we replace

--- List & Tuple/insert_more_items.py
+++ List & Tuple/insert_more_items.py
@@ -5,3 +5,10 @@ mylist = ["apple", "banana", "cherry"]
 mylist[1:2] = ["mango", "peach"]
 
 print(mylist)
+
+### if we insert less items than we replace, the new items will be inserted where we we specified, and the remaining items will move accordingly.
+
+mynewlist = ["apple", "banana", "cherry"]
+mynewlist[1:3] = ["orange"]
+
+print(mynewlist)
