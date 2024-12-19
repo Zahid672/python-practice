cb6a4104969fdd27a3f2386e551a5b24c482d0e6
Zahid672 <zeeuom@gmail.com>
2024-12-19 16:58:23 +0900

    extend method to append another element from another list to the current list.

---
+++ List & Tuple/extendlist.py
@@ -0,0 +1,8 @@
+## To append elements from another list to the current list, we use the extend() method.
+
+mylist = ["Banana", "Apple", "orange"]
+
+newlist = ["Burger", "Lamb"]
+
+mylist.extend(newlist)
+print(mylist)
