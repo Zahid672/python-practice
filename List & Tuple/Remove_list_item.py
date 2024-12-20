d0e654d531a7b4d029447968c259f36e5e8bd2fc
Zahid672 <zeeuom@gmail.com>
2024-12-20 16:36:01 +0900

    remove() method removes the specified item.

---
+++ List & Tuple/Remove_list_item.py
@@ -0,0 +1,13 @@
+## The remove method removes the specified the specified item.
+
+mylist = ["First", "Second", "Third"]
+
+mylist.remove("First")
+
+print(mylist)
+
+### if there are more than one item with the specified value, the remove() method removes the first occurrence.
+
+mylist1 = ["apple", "apple", "banana", "orange", "orange"]
+mylist1.remove("apple")
+print(mylist1)
