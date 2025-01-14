43dfc75feede080cbca141dfd39da51c83642163
Zahid672 <zeeuom@gmail.com>
2025-01-14 16:32:45 +0900

    How to add property to a class

---
+++ OOPS/Add_properties.py
@@ -0,0 +1,17 @@
+## Add a property called graduationyear to the student class:
+
+class Person:
+    def __init__(self, fname, lname):
+        self.firstname = fname
+        self.lastname = lname
+        
+    def printname(self):
+        print(self.firstname, self.lastname)
+        
+class Student(Person):
+    def __init__(self, fname, lname):
+        super().__init__(fname, lname)
+        self.graduationyear = 2019
+        
+x = Student("Atif", "Niaz")
+print(x.graduationyear)
