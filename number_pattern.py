915ae08d2a6902c3fde05993694984fb2ef9cc53
Zahid Ullah <44077900+Zahid672@users.noreply.github.com>
2025-07-24 10:47:12 +0900

    sum of all numbers from 1 to a given number

--- number_pattern.py
+++ number_pattern.py
@@ -1,6 +1,6 @@
 # Write a Python code to print the following number pattern using a loop.
-n = 5
-for i in range(1, n + 1):
+n = 20
+for i in range(1, n + 1, 5):
     for j in range(1, i + 1):
         print(j, end=' ')
     print()
