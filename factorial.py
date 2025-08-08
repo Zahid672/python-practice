549193242b05377698a9fb3793d86a404ba2c509
Zahid Ullah <44077900+Zahid672@users.noreply.github.com>
2025-08-08 10:26:09 +0900

    Factorial

---
+++ factorial.py
@@ -0,0 +1,11 @@
+num = 5
+factorial = 1
+if num < 0:
+    print("Factorial is not defined for negative numbers.")
+elif num == 0:
+    print("Factorial of 0 is 1")
+else:
+    for i in range(1, num + 1):
+        factorial *= i
+    print(f"Factorial of {num} is {factorial}")
+      
