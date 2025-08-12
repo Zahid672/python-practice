d0919777bf12d77b88be13634afd157792551f4c
Zahid Ullah <44077900+Zahid672@users.noreply.github.com>
2025-08-12 10:14:59 +0900

    using list

--- odd_position.py
+++ odd_position.py
@@ -1,6 +1,8 @@
 my_list = [10,20, 30, 40, 50, 60, 70, 80, 90, 100]
-odd_list = []
-for i in range(len(my_list)):
-    if i % 2!= 0:
-        odd_list.append(my_list[i])
-print(odd_list)
+# odd_list = []
+# for i in range(len(my_list)):
+#     if i % 2!= 0:
+#         odd_list.append(my_list[i])
+# print(odd_list)
+
+print(my_list[1::2])  # This will print elements at odd index positions
