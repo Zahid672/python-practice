0eac21872ba477d47a5959b14631bef392e4f8b7
Zahid Ullah <44077900+Zahid672@users.noreply.github.com>
2025-09-04 10:09:45 +0900

    function with a default argument

---
+++ default_arg.py
@@ -0,0 +1,8 @@
+def show_empoyee(name, salary = 9000):
+    print("Name:", name, "Salary:", salary)
+    return show_empoyee
+
+emp_1 = show_empoyee("Khan", 8000)
+emp_2 = show_empoyee("John")  # salary will use the default value
+print(emp_1)  # This will print the function itself
+print(emp_2)  # This will also print the function itself
