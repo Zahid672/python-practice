649a4379a272ae677e431f241f470f630f9a9eed
Zahid Ullah <44077900+Zahid672@users.noreply.github.com>
2025-09-04 09:59:31 +0900

    Return multiple values

--- Rev_int_num.py
+++
@@ -1,9 +0,0 @@
-# num = 24567
-# print(str(num)[-1:-6:-1])
-
-rev_num = 0
-num = 24567
-while num > 0:
-    rev_num = rev_num * 10 + num % 10
-    num //= 10
-print(rev_num)
--- While_loop.py
+++
@@ -1,5 +0,0 @@
-natural_numbers = 10
-count = 1
-while count <= natural_numbers:
-    print(count)
-    count += 1
