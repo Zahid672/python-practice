2e6ae4982ca11d734e824fc79f0fc2859ebd1e2a
Zahid Ullah <zeeuom@gmail.com>
2025-12-01 14:57:12 +0900

    list operations pop, insert, append

--- Replace_symbols.py
+++
@@ -1,6 +0,0 @@
-str1 = '/Jon is @developer & musician!!'
-symbols = ['@', '&', '!', '/']
-
-for symbol in symbols:
-    str1 = str1.replace(symbol, '#')
-print(str1)
--- String_Exercise/substring_occurences.py
+++
@@ -1,7 +0,0 @@
-str1 = "Welcome to Korea. korea awesome. isn't it?"
-sub_string = "Korea"
-
-temp_str = str1.lower()
-
-count = temp_str.count(sub_string.lower())
-print("The Korea count is:", count)
