1f23f00eeb3959e59627cad491a251d804de14cc
Zahid Ullah <zeeuom@gmail.com>
2026-01-14 11:10:16 +0900

    Sum and average of all numbers in a list

---
+++ List/sum_avg.py
@@ -0,0 +1,7 @@
+my_list = [10, 20, 30, 40, 50]
+print("Original List:", my_list)
+total = sum(my_list)
+print("Sum of elements:", total)
+
+average = total / len(my_list)
+print("Average of elements:", average)
